30cd880461780f3f905f1a4522a9025decdbc66c
hacksman <funblessu@gmail.com>
2018-05-02 16:05:50 +0800

    commit chart_rnn model

---
+++ sample.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# @Time    : 4/27/18 8:34 AM
+
+import tensorflow as tf
+from read_utils import TextCoverter
+from models import CharRNN
+import os
+from IPython import embed
+
+FLAGS = tf.flags.FLAGS
+
+tf.flags.DEFINE_integer('lstm_size', 128, 'size of hidden state of lstm')
+tf.flags.DEFINE_integer('num_layers', 2, 'number of lstm layers')
+tf.flags.DEFINE_boolean('use_embedding', False, 'whether to use embedding')
+tf.flags.DEFINE_integer('embedding_size', 128, 'size of embedding')
+tf.flags.DEFINE_string('converter_path', 'model/poetry/converter.pkl', 'model/name/converter.pkl')
+tf.flags.DEFINE_string('checkpoint_path', 'model/poetry', 'checkpoint path')
+tf.flags.DEFINE_string('start_string', u'月', 'use this string to start generating')
+tf.flags.DEFINE_integer('max_length', 50, 'max length to generate')
+
+
+def main(_):
+    FLAGS.start_string = FLAGS.start_string
+    converter = TextCoverter(filename=FLAGS.converter_path)
+    if os.path.isdir(FLAGS.checkpoint_path):
+        FLAGS.checkpoint_path = tf.train.latest_checkpoint(FLAGS.checkpoint_path)
+
+    model = CharRNN(converter.vocab_size, sampling=True,
+                    lstm_size=FLAGS.lstm_size, num_layers=FLAGS.num_layers,
+                    use_embedding=FLAGS.use_embedding,
+                    embedding_size=FLAGS.embedding_size,
+                    )
+
+    model.load(FLAGS.checkpoint_path)
+
+    start = converter.text_to_arr(FLAGS.start_string)
+    arr = model.sample(FLAGS.max_length, start, converter.vocab_size)
+    print(converter.arr_to_text(arr))
+
+if __name__ == '__main__':
+    tf.app.run()
